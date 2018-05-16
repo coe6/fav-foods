725ae27cd3a0a07fc408dd3ef994a45b3aad7491
Sam Coe <coe6@purdue.edu>
2018-05-16 14:39:42 -0400

    Change from food to movies and add dropdown menus

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 
-function renderListItem() {
+let movieArray = []
 
+function createDeleteBttn(list) {
+    const bttn = document.createElement('button')
+    bttn.textContent = 'Remove'
+
+    list.appendChild(bttn)
+}
+
+function deleteItem(ev) {
+    
+}
+
+function renderListItem(type, value) {
+    const item = document.createElement('li')
+    item.textContent = `${type} ${value}`
+
+    return item
 }
 
 function renderList(form) {
+    const list = document.createElement('ul')
+
+    const movieItem = {
+        'Title: ': form.movie.value,
+        'Genre: ': form.genres.value,
+        'Currently: ': form.watch.value,
+    }
+    const tempArray = Object.keys(movieItem).map(value => {
+         const item = renderListItem(value, movieItem[value])
+         list.appendChild(item)
+
+        return movieItem[value]
+     })
+
+     createDeleteBttn(list)
+
+     tempArray.map(value => {
+         movieArray.push(value)
+     })
+
+     return list;
 
 }
 
@@ -11,13 +48,13 @@ function handleSubmit(ev) {
     ev.preventDefault()
 
     const form = ev.target
-    const foods = document.querySelector('#foods')
+    const movies = document.querySelector('#movies')
 
-    foods.appendChild(renderList(form))
+    movies.appendChild(renderList(form))
 
     form.reset()
-    form.food.focus()
+    form.movie.focus()
 }
 
-const form = document.querySelector('form#foodInput')
+const form = document.querySelector('form#movieInput')
 form.addEventListener('submit', handleSubmit)
